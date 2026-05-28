7e297fea844f3318e6757cafdea5f82ecf6e1e48
logan <92327020+DevLoggith@users.noreply.github.com>
2026-05-28 09:31:32 -0400

    create conditional to display current move instead of button
    closes #1

--- src/App.js
+++ src/App.js
@@ -69,14 +69,17 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
 
-    if (move > 0) {
+    if (move === currentMove) {
+      description = "You are at move #" + move;
+    } else if (move > 0) {
       description = "Go to move #" + move;
     } else {
-      description = "Go to game start"
+      description = "Go to game start";
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move !== currentMove ? (<button onClick={() => jumpTo(move)}>{description}</button>) : description}
+        
       </li>
     );
   });
