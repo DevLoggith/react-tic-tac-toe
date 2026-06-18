c2fc1cf0799c1ddac4272347ef8f9dd8412e3a87
logan <92327020+DevLoggith@users.noreply.github.com>
2026-06-18 19:14:06 -0400

    display tie game message when there's no winner
    if the board is full, yet there was no winner
    declared, the only conclusion is that it's a tie
    game.

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function Board({ xIsNext, squares, onPlay }) {
 	let status;
 	if (winner) {
 		status = "Winner: " + winner;
+	} else if (!squares.includes(null)) {
+		status = "Cat's game";
 	} else {
 		status = "Next player: " + (xIsNext ? "X" : "O");
 	}
